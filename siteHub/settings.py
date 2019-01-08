be93bf4b421aafae5254f0a7d5f47be8805bfb5c
edriano <edriano_zanatta@hotmail.com>
2019-01-08 20:45:25 -0200

    test4

--- siteHub/settings.py
+++ siteHub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(fx@ydbrh!1om^cshfzql7=l&+85s&tryl^8am49rn(0yr!9$c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['68.183.105.161']
 
