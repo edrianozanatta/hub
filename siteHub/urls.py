42e7f9df8e9947250dc2acc6033f6d92badc7b52
edriano <edriano_zanatta@hotmail.com>
2019-01-09 10:05:16 -0200

    teste7

--- siteHub/urls.py
+++ siteHub/urls.py
@@ -20,7 +20,10 @@ from django.urls import path
 from siteHub.hub import urls as site_urls
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include(site_urls, namespace='site')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
